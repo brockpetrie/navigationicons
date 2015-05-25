2aaece526d11de8f2d7bf8f31c102e4ea68a8c0b
Nicolas Brassard <nico@nitriques.com>
2015-05-25 15:12:06 -0400

    Fix encoding and broken sort
    This fixes multiples issue when the config were saved. The were some problems with the json encode/decode calls and the Configuration::write which call addslashes.
    
    This should make things ok since we re-parse the value in the config on each request.

--- extension.driver.php
+++ extension.driver.php
@@ -29,11 +29,15 @@
 			);
 		}
 
+	/*-------------------------------------------------------------------------
+		Install:
+	-------------------------------------------------------------------------*/
+
 		public function install() {
 			// Set default icons
 			// The 3 included in the webfont with this extension are the dashboard, puzzle piece (blueprints) and settings (system) icons.
 			// Saved to the config as a JSON-encoded array
-			$config = json_encode(array('Blueprints' => 'blueprints', 'System' => 'system', 'Dashboard' => 'dashboard'));
+			$config = static::encode(array('Blueprints' => 'blueprints', 'System' => 'system', 'Dashboard' => 'dashboard'));
 			Symphony::Configuration()->set('navigation', $config, 'navigationicons');
 			Symphony::Configuration()->write();
 		}
@@ -43,20 +47,55 @@
 			Symphony::Configuration()->write();
 		}
 
+	/*-------------------------------------------------------------------------
+		Utils:
+	-------------------------------------------------------------------------*/
+
+		public static function encode($array) {
+			return json_encode($array);
+		}
+
+		public static function decode($string, $asArray = false) {
+			$string = stripslashes($string);
+			$string = str_replace('\\', '', $string);
+			$string = @json_decode($string, $asArray);
+			if (!$string) {
+				return array();
+			}
+			return $string;
+		}
+
+		private static function getAuthor() {
+			$author = null;
+			if (is_callable(array('Symphony', 'Author'))) {
+				$author = Symphony::Author();
+			} else {
+				$author = Administration::instance()->Author;
+			}
+			return $author;
+		}
 
 	/*-------------------------------------------------------------------------
 		Delegates:
 	-------------------------------------------------------------------------*/
 
 		public function appendAssets() {
-			$config = json_decode(Symphony::Configuration()->get('navigation', 'navigationicons'), true);
-			foreach ($config as $key => $value) {
-				$config[__($key)] = __($value);
+			$config = static::decode(Symphony::Configuration()->get('navigation', 'navigationicons'), true);
+			if (is_array($config)) {
+				foreach ($config as $key => $value) {
+					$config[__($key)] = __($value);
+				}
+			} else {
+				$config = array();
 			}
-			$script = new XMLElement('script', __('var navigationArr = '. json_encode($config).';'), array('type' => 'text/javascript'));
+			$config = static::encode($config);
+			$script = new XMLElement('script', __('var navigationArr = '. $config  .';'), array('type' => 'text/javascript'));
 			Administration::instance()->Page->addElementToHead($script, 1000, true);
 			Administration::instance()->Page->addScriptToHead(URL . '/extensions/navigationicons/assets/navigationicons.js', 1001, false);
 			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/navigationicons/assets/navigationicons.css', 'screen', 1002, false);
+			// Reset the value in the array, to prevent fighting against
+			// the Configuration::write method which add slashes
+			Symphony::Configuration()->set('navigation', $config, 'navigationicons');
 		}
 
 		public function parseNav($context) {
@@ -69,7 +108,7 @@
 		}
 
 		public function appendPreferences($context) {
-			$config = json_decode(Symphony::Configuration()->get('navigation', 'navigationicons'));
+			$config = static::decode(Symphony::Configuration()->get('navigation', 'navigationicons'), true);
 
 			Administration::instance()->Page->addScriptToHead(URL . '/extensions/navigationicons/assets/navigationicons.preferences.js', 4000, false);
 
@@ -78,15 +117,24 @@
 			$fieldset->appendChild(new XMLElement('legend', __('Navigation Icons')));
 			//$fieldset->appendChild(new XMLElement('p', __(''), array('class' => 'help')));
 
-			$div = new XMLElement('span', null, array('class' => 'frame'));
+			if (self::getAuthor()->get('language') != 'en') {
+				$div = new XMLElement('div', null, array('class' => 'frame'));
+				$p = new XMLElement('h2', __('You can not edit navigations icons if not in English'));
+				$div->appendChild($p);
+				$fieldset->appendChild($div);
+				$context['wrapper']->appendChild($fieldset);
+				return;
+			}
+
+			$div = new XMLElement('div', null, array('class' => 'frame'));
 			$duplicator = new XMLElement('ol');
 			$duplicator->setAttribute('class', 'navigationicons-duplicator');
 
 			foreach ($this->navItems as $i=>&$item) {
 				$li = new XMLElement('li');
-				$li->appendChild(new XMLElement('label', __($item)));
+				$li->appendChild(new XMLElement('label', $item));
 
-				$val = array_key_exists($item, $config) ? $config->$item : '';
+				$val = array_key_exists($item, $config) ? $config[$item] : '';
 				$navLabel = new XMLElement('span', $item, array('data-icon' => $val));
 				$iconInput = Widget::Input('settings[navigationicons][navigation][navitem'. $i .'][]', $val, 'text', array('class' => 'navIcon', 'placeholder' => 'Ligature or symbol'));
 				$labelInput = Widget::Input('settings[navigationicons][navigation][navitem'. $i .'][]', $item, 'hidden');
@@ -110,7 +158,7 @@
 				if ($item[0] != '') $config[$item[1]] = $item[0];
 			}
 			//print_r($config);
-			$context['settings']['navigationicons']['navigation'] = json_encode($config);
+			$context['settings']['navigationicons']['navigation'] = static::encode($config);
 		}
 
 
